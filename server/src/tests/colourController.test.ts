142f7444f9af42420ab467aecd7c889944c2214e
Nourdine Mazali <nourdine@archistar.ai>
2023-02-15 11:44:10 +1100

    Unit tests for ColourController added

---
+++ server/src/tests/colourController.test.ts
@@ -0,0 +1,57 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import Colour from "../models/colour";
+import request from "supertest";
+import express, { Application } from 'express';
+import cors from "cors";
+import coloursRouter from "../routes/colours";
+
+describe("/colours tests", () => {
+  let mongod: MongoMemoryServer;
+  let app: Application;
+  let id: string;
+  let colour;
+
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
+    await mongoose.connect(uri);
+    app = express();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(coloursRouter);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+  });
+
+  beforeEach(async () => {
+    colour = new Colour({
+      name: "test",
+    });
+    await colour.save();
+    id = colour._id.toString();
+  });
+
+  afterEach(async () => {
+    await Colour.deleteMany({});
+  });
+
+  it("should get colours", async () => {
+    const response = await request(app).get("/colours");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      colours: [
+        expect.objectContaining({
+          _id: id,
+          name: "test",
+        }),
+      ],
+    });
+  });
+});
